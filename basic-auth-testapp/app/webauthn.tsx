640ddc5f2d263ffbebb4ea2a5f9a778a13ac587a
Khoi Nguyen <koinguyen@ucdavis.edu>
2023-11-16 17:11:16 -0800

    Continue with the webauthn registration flow

--- basic-auth-testapp/app/webauthn.tsx
+++ basic-auth-testapp/app/webauthn.tsx
@@ -1,40 +1,8 @@
 'use client'
 
-import { create, parseCreationOptionsFromJSON } from "@github/webauthn-json/browser-ponyfill"
+import { RegistrationPublicKeyCredential, create, parseCreationOptionsFromJSON } from "@github/webauthn-json/browser-ponyfill"
 
 const hankoApi = process.env.NEXT_PUBLIC_HANKO_API_URL
-
-function printObject (obj: any, indent = 0) {
-    const indentation = '  '.repeat(indent);
-
-    for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            const value = obj[key];
-            const valueType = typeof value;
-
-            console.log(`${indentation}${key}: ${valueType}`);
-
-            if (valueType === 'object') {
-                // Recursively print nested objects
-                printObject(value, indent + 1);
-            } else {
-                console.log(`${indentation}  ${value}`);
-            }
-        }
-    }
-}
-
-// Example usage:
-const sampleObject = {
-    name: 'John',
-    age: 30,
-    address: {
-        city: 'Example City',
-        zip: '12345'
-    },
-    hobbies: ['reading', 'coding']
-};
-
 
 // Use x-auth-token instead of cookie to work with chrome not allowing cross-domain cookies
 const webauthnInit = async () => {
@@ -54,11 +22,34 @@ const webauthnInit = async () => {
     return creationOptions
 }
 
+// Send the public key generate by the browser and user authenticator
+const webAuthnFinalize = async (cred: RegistrationPublicKeyCredential) => {
+    const xAuthToken = sessionStorage.getItem('hanko') || ''
+    const response = await fetch(`${hankoApi}/webauthn/registration/finalize`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${xAuthToken}`
+        },
+        credentials: "include",
+        body: JSON.stringify(cred.toJSON())
+    })
+
+    console.log('Finalize registration result')
+    console.table(response)
+}
+
 export const webAuthnRegistration = async () => {
     const creationOption = await webauthnInit()
     const parsedOptions = parseCreationOptionsFromJSON(creationOption)
     const credentials = await create(parsedOptions)
 
+
     console.log(`The credentials Created is`)
     console.table(credentials)
+
+    console.log('The better credentials printout')
+    const credJson = await credentials.toJSON()
+    console.table(credJson)
+
+    await webAuthnFinalize(credentials)
 }
